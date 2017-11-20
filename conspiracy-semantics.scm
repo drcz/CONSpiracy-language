d2d18e92abab24767c901d9bbc36b6f7604d170c
dercz <drcz@o2.pl>
2017-11-20 04:37:29 +0100

    re-scoped desugar and FV so they share the same error 'handler' (because using set! is equivalent to 7 years of broken mirrors)

--- conspiracy-semantics.scm
+++ conspiracy-semantics.scm
@@ -1,11 +1,10 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; The [operational] semantics of CONSpiracy, aka the Thing.
-;;; 2017-11-18/19, Alicante
-
+;;; 2017.11.18-20, Alicante
 (include "conspiracy-syntax.scm") ;; TODO: sure?
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; "environments"...
+;;; "environments in general"...
 
 (define binding? ;;; FYI
   (list-of (lambda ((k . v)) (and (identifier? k)
@@ -23,74 +22,9 @@
   `([,id . ,expr] . ,(alist-delete id binding)))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; internal representation of phi-forms...
+;;; + internal representation of phi-forms with their enclosed bindings...
 (define (closure? x)
   (and-let* ([('&CLOSURE ([(? pattern?) (? form?)] ...) (? binding?)) x])))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; notion of free variable.
-
-(define (free-vars form)
-  (define (pattern-bound-vars pattern)
-    (match pattern
-      [(? identifier? id) `(,id)]
-      [('quote _) '()]
-      [('? _) '()]
-      [('? _ id) `(,id)]
-      [(p . ps) (union (pattern-bound-vars p) (pattern-bound-vars ps))]
-      [_ '()]))
-
-  (define (guards-free-vars pattern)
-    (match pattern
-      [(? identifier?) '()]
-      [('quote _) '()]
-      [('? guard . _) (free-vars guard)]
-      [(p . ps) (union (guards-free-vars p) (guards-free-vars ps))]
-      [_ '()]))
-
-  (match form
-    [(? identifier? id) `(,id)]
-    
-    [(? closure?) '()] ;; just think about it.
-
-    [('phi . cases) (let fv-cases ([cases cases])
-                    (match cases
-                      [() '()]
-                      [([pattern* form*] . cases*)
-                       (union (difference (union (free-vars form*)
-                                                 (guards-free-vars pattern*))
-                                          (pattern-bound-vars pattern*))
-                              (fv-cases cases*))]
-                      [wtf? (error `(deformed phi form case ,wtf?))]))]
-
-    [('quote _) '()]
-
-    [('quasiquote qqe) (let fv-qq ([expr qqe])
-                         (match expr
-                           [('unquote e) (free-vars e)]
-                           [(h . t) (union (fv-qq h) (fv-qq t))]
-                           [_ '()]))]
-
-    [('if p c a) (union (free-vars p) (free-vars c) (free-vars a))]
-
-    [(f . (? list? args)) (apply union (map free-vars form))]
-    [(f . a) (union (free-vars f) (free-vars a))]
-
-    [_ '()]))
-
-(e.g. (free-vars '`(,x ,(f y (+ x y) z))) ===> (f y + z x))
-(e.g. (free-vars '(phi [(x) (* x x)])) ===> (*))
-(e.g. (free-vars '(phi [(x) (* x y)])) ===> (y *))
-
-(e.g. (free-vars '(phi [(x) (* x y)]
-                     [(z y) (+ z y)]
-                     [(z x) (f z x abc)])) ===> (+ abc f y *))
-
-(e.g. (free-vars '(phi [(? (phi [(x) (p? x)])) 'qwe])) ===> (p?))
-(e.g. (free-vars '(phi [((? p? x) (? q?)) `(,x ,xyz)])) ===> (p? q? xyz))
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; the evaluator.
@@ -99,6 +33,7 @@
                      #;in-the-context-of defined 
                      #;on-error-applying error)
 
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   (define (value form binding)
     (match form ;;; nb we assert desugared form here.
 
@@ -135,7 +70,7 @@
 
       [_ (error `(unrecognized form ,form))]))
 
-
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   (define (application rator rands local-binding)
     (match rator
 
@@ -157,7 +92,7 @@
 
       [_ (error `(unrecognized application ,rator))]))
 
-
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   (define (matching pattern form #;wrt match-bindings)
     (let matching ([pattern pattern]
                    [form form]
@@ -194,7 +129,7 @@
 
         [_ (error `(unrecognized pattern ,pattern))])))
 
-
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   (define (primop-application app)
     (match app
 
@@ -254,11 +189,139 @@
 
       [_ (error `(unrecognized primitive application ,app))]))
 
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;;; the notion of free variable...
+  (define (free-vars form)
+    (define (pattern-bound-vars pattern)
+      (match pattern
+        [(? identifier? id) `(,id)]
+        [('quote _) '()]
+        [('? _) '()]
+        [('? _ id) `(,id)]
+        [(p . ps) (union (pattern-bound-vars p) (pattern-bound-vars ps))]
+        [_ '()]))
+
+    (define (guards-free-vars pattern)
+      (match pattern
+        [(? identifier?) '()]
+        [('quote _) '()]
+        [('? guard . _) (free-vars guard)]
+        [(p . ps) (union (guards-free-vars p) (guards-free-vars ps))]
+        [_ '()]))
+
+    (match form
+      [(? identifier? id) `(,id)]
+      
+      [(? closure?) '()] ;; just think about it.
+
+      [('phi . cases) (let fv-cases ([cases cases])
+                      (match cases
+                        [() '()]
+                        [([pattern* form*] . cases*)
+                         (union (difference (union (free-vars form*)
+                                                   (guards-free-vars pattern*))
+                                            (pattern-bound-vars pattern*))
+                                (fv-cases cases*))]
+                        [wtf? (error `(deformed phi form case ,wtf?))]))]
+
+      [('quote _) '()]
+
+      [('quasiquote qqe) (let fv-qq ([expr qqe])
+                           (match expr
+                             [('unquote e) (free-vars e)]
+                             [(h . t) (union (fv-qq h) (fv-qq t))]
+                             [_ '()]))]
+
+      [('if p c a) (union (free-vars p) (free-vars c) (free-vars a))]
+
+      [(f . (? list? args)) (apply union (map free-vars form))]
+      [(f . a) (union (free-vars f) (free-vars a))]
+
+      [_ '()]))
+
+  #;(e.g. (free-vars '`(,x ,(f y (+ x y) z))) ===> (f y + z x))
+  #;(e.g. (free-vars '(phi [(x) (* x x)])) ===> (*))
+  #;(e.g. (free-vars '(phi [(x) (* x y)])) ===> (y *))
+
+  #;(e.g. (free-vars '(phi [(x) (* x y)]
+                       [(z y) (+ z y)]
+                       [(z x) (f z x abc)])) ===> (+ abc f y *))
+
+  #;(e.g. (free-vars '(phi [(? (phi [(x) (p? x)])) 'qwe])) ===> (p?))
+  #;(e.g. (free-vars '(phi [((? p? x) (? q?)) `(,x ,xyz)])) ===> (p? q? xyz))
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;;; one more thing, desugaring the syntax {perhaps not the best choice}
+  (define (desugared form)
+    (match form
+      ;;; and/or/not are just sugar for ifs (TODO: for now?)
+      [('and . fs*)
+       (fold-right (lambda (f* fs) (let ([f (desugared f*)])
+                                (if fs `(if ,f ,fs #f) f)))
+                   #f
+                   fs*)]
+      [('or . fs*)
+       (fold-right (lambda (f* fs) (let ([f (desugared f*)])
+                                (if fs `(if ,f #t ,fs) f)))
+                   #f
+                   fs*)]
+      [('not f*)
+       `(if ,(desugared f*) #f #t)]
+ 
+      ;;; let's are just sugar for nested phi-forms (TODO: for now?)
+      [('let bnds* f*)
+       (fold-right (lambda ((pat frm*) bs) `((phi [(,pat) ,bs]) ,(desugared frm*)))
+                   (desugared f*)
+                   bnds*)]
+      
+      ;;; propagate desugaring into subforms...
+      [(? phi-form?)
+       (let* ([('phi . cases) form])
+         `(phi . ,(map (lambda ((p* f*)) `(,(desugared-pattern p*)
+                                    ,(desugared f*))) cases)))]
+
+      [('if p* c* a*)
+       `(if ,(desugared p*) ,(desugared c*) ,(desugared a*))]
+
+      [('quasiquote qqe*)
+       `(,'quasiquote ,(let desugared-qq ([expr qqe*])
+                         (match expr
+                           [('unquote e) `(,'unquote ,(desugared e))]
+                           [(h . t) `(,(desugared-qq h) . ,(desugared-qq t))]
+                           [_ expr])))]
+
+      [('quote _) form]
+
+      [(f* . as*)
+       `(,(desugared f*) . ,(if (list? as*) (map desugared as*) (desugared as*)))]
+
+      ;;; gucci.
+      [_ form]))
+
+  ;;; because of guard expressions, patterns need to get desugared as well!
+  (define (desugared-pattern pattern)
+    (match pattern
+      [('? (? form? f*)) `(? ,(desugared f*))]
+      [('? (? form? f* id)) `(? ,(desugared f*) id)]
+      [(h . t) `(,(desugared-pattern h) . ,(desugared-pattern t))]
+      [_ pattern]))
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  #;[e.g. (desugared '(phi [(x) (* x x)])) ===> (phi [(x) (* x x)])]
+  #;[e.g. (desugared '(phi [(x) (* x x)] [(x y) (+ x y)]))
+        ===> (phi [(x) (* x x)] [(x y) (+ x y)])]
+  #;[e.g. (desugared '`(q w e (not important) ,(let ([(y . ys) x]
+                                                   [x (f x)])
+                                               (and a b c))))
+        ===> `(q w e (not important)
+               ,[(phi [((y . ys)) [(phi [(x) (if a (if b c #f) #f)]) (f x)]]) x])]
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
    ;;; ...and finally:
   (value (desugared form) '()))
 
 
-(define (default-initial-environment)
+(define (default-initial-environment) ;; XD
   `([atom? . &atom?]
     [string? . &str?]
     [numeral? . &num?]
